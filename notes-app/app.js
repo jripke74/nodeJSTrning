8d61c537ab3c59f41feedab04857c02677fce727
jripke74@gmail.com <jripke74@gmail.com>
2019-12-23 09:58:09 -0800

    add new files for JS trning

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,57 @@
-const fs = require('fs')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js')
+// Customize yarg version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    console.log(`Title: ${argv.title}`)
+    console.log(`Body: ${argv.body}`)
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Removing the note')
+  }
+})
+
+// List the notes
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: () => {
+    console.log('Listing out all notes')
+  }
+})
+
+// Read a note
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Reading a note')
+  }
+})
+
+yargs.parse()
